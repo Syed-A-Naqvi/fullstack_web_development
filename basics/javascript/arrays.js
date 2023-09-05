184632249bf077d4e450c3acf89476d1f4d8e875
Syed Arham Naqvi <syedm.naqvi@ontariotechu.net>
2023-09-04 22:06:05 -0400

    practiced arrays in js

---
+++ basics/javascript/arrays.js
@@ -0,0 +1,39 @@
+// arrays work as expected although the console.log() function is able to print the array as a string without the need for any manual concatenation
+//declaring arrays
+let ourArray = ["John", 23];
+let myArray = ["Quincy", 1, "cat"];
+let yourArray = ["dog", 2, "cat"];
+let nestedArray = [ourArray, myArray, yourArray];
+
+console.log(ourArray);
+console.log(nestedArray);
+
+//accessing arrays
+console.log(ourArray[0]);
+console.log(nestedArray[1]);
+
+//modifying arrays
+//arrays are mutable and can be modified after their declaration using the index of the element to be modified
+ourArray[1] = 5;
+console.log(ourArray);
+
+//consider accessing the following nested array
+let myData = [ [1,2,3], [[4,5,6], 7, 8, 9] ];
+console.log(myData[0]);
+//we are accessing the number 4 which is the first element of an array that is nested in an array which is the first element of an array that is the second element of the myData array
+console.log(myData[1][0][0]);
+
+//push() is used to append data to the end of an array
+myData.push(10);
+console.log(myData);
+//pop() is used to remove the last element of an array
+let removed = myData.pop([10]);
+console.log(myData);
+console.log(removed);
+//shift() is used to remove the first element of an array
+let shifted = myData.shift();
+console.log(myData);
+console.log(shifted);
+//unshift() is used to add an element to the beginning of an array
+myData.unshift(123);
+console.log(myData);
---
+++ basics/javascript/wordBlank.js
@@ -0,0 +1,13 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+
+    // Your code below this line
+    let result = "My " + myAdjective + " " + myNoun + " decided to " + myVerb + " " + myAdverb + "." ;
+
+    // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+console.log(wordBlanks("dog", "stupid", "bark", "unusually"));
+console.log(wordBlanks("molerat", "ugly", "jiggle", "absurdly"));
+console.log(wordBlanks("cat", "fat", "sleep", "outragously"));
