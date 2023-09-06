8ff9632dc46b7851ec71bd9cfd7afe9c3ea236c0
Syed Arham Naqvi <syedm.naqvi@ontariotechu.net>
2023-09-05 22:35:48 -0400

    more javascript work

---
+++ basics/javascript/functions.js
@@ -0,0 +1,10 @@
+// will return a random floating point value between 0 and 1 inclusive of 0 but not 1
+function randomFraction(){
+    return Math.random();
+}
+
+// will return a random whole number between 0 and passed parameter
+function randomWholeNum(num){
+    return Math.floor(Math.random() * num);
+}
+
